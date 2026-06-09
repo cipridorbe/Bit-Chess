565d4aa634fc39ed2b8741a41769c76cbdcda2a5
cipridorbe <cipridorbe@gmail.com>
2026-06-09 18:20:15 -0400

    Implemented quiescence search

--- src/search/negamax.rs
+++ src/search/negamax.rs
@@ -4,11 +4,13 @@ Implementation of negamax, used as the main search algorithm.
 
 use crate::{bitboard::{Board, Piece}, eval::relative_eval, movegen::{attacks::{all_attacks, is_in_check}, generator::generate_movelist, makemove::{is_in_check_after_move, make_move, unmake_move}, r#move::Move}};
 
-pub fn search(board: &mut Board,depth: u8) -> Option<Move> {
+const MATE_VAL: f32 = 1000000.;
+
+pub fn search(board: &mut Board, depth: u8) -> Option<Move> {
     let mut best_score = f32::NEG_INFINITY;
     let mut best_move = None;
 
-    let mut movelist = generate_movelist(&board);
+    let mut movelist = generate_movelist(&board, false);
     movelist.sort_mvvlva(&board);
     for mv in movelist.iter() {
         let unmake = make_move(board, mv);
@@ -33,10 +35,10 @@ pub fn search(board: &mut Board,depth: u8) -> Option<Move> {
 
 pub fn negamax(board: &mut Board, mut alpha: f32, beta: f32, depth: u8) -> f32 {
     if depth == 0 {
-        return relative_eval(board);
+        return quiescence(board, alpha, beta);
     }
 
-    let mut movelist = generate_movelist(board);
+    let mut movelist = generate_movelist(board, false);
     movelist.sort_mvvlva(board);
     let mut value = f32::MIN;
     let mut moved = false;
@@ -62,12 +64,53 @@ pub fn negamax(board: &mut Board, mut alpha: f32, beta: f32, depth: u8) -> f32 {
     if !moved {
         let attacks = all_attacks(board, board.side.other());
         if is_in_check(attacks, board.pieces[Piece::king(board.side) as usize]) {
-            // checkmate
-            return -(1000000. + depth as f32);
+            return -(MATE_VAL + depth as f32);
         } else {
-            // stalemate
             return 0.;
         }
     }
-    return value;
+    value
+}
+
+fn quiescence(board: &mut Board, mut alpha: f32, beta: f32) -> f32 {
+    let attacks = all_attacks(board, board.side.other());
+    let in_check = is_in_check(attacks, board.pieces[Piece::king(board.side) as usize]);
+    let stand_pat = if !in_check {
+        let eval = relative_eval(board);
+        if eval >= beta {
+            return eval;
+        }
+        alpha = f32::max(alpha, eval);
+        eval
+    } else {
+        f32::MIN
+    };
+
+    let mut value = stand_pat;
+    let mut movelist = generate_movelist(board, !in_check);
+    let mut moved = false;
+    movelist.sort_mvvlva(board);
+    for mv in movelist.iter() {
+        let unmake = make_move(board, mv);
+        if is_in_check_after_move(board) {
+            unmake_move(board, mv, unmake);
+            continue;
+        }
+        moved = true;
+        let score = if board.is_rule_draw() {
+            0.
+        } else {
+            -quiescence(board, -beta, -alpha)
+        };
+        unmake_move(board, mv, unmake);
+        value = f32::max(value, score);
+        alpha = f32::max(alpha, value);
+        if alpha >= beta {
+            break;
+        }
+    }
+    if !moved && in_check {
+        return -(MATE_VAL / 2. - board.fullmoves as f32)
+    }
+    value
 }
